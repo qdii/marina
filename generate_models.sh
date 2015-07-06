e2670c2686f48069149c0268923e25d1b665fa14
Victor Lavaud (qdii) <victor.lavaud@gmail.com>
2015-07-06 14:49:57 +0200

    updated models with comments

--- generate_models.sh
+++ generate_models.sh
@@ -3,5 +3,5 @@ for i in boat composition cruise dish ingredient meal unit
 do
     capitalized_name=$(echo $i | sed 's/./\U&\E/')
     echo Creating model for: $i
-    ${base_dir}/yii gii/model --overwrite=1 --tableName="$i" --interactive=0 --generateRelations=1 --enableI18N=1 --modelClass="$capitalized_name"
+    ${base_dir}/yii gii/model --generateLabelsFromComments=1 --overwrite=1 --tableName="$i" --interactive=0 --generateRelations=1 --enableI18N=1 --modelClass="$capitalized_name"
 done

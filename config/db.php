20a274513882cf9b226609a8cefa7faacbd23993
Victor Lavaud (qdii) <victor.lavaud@gmail.com>
2015-07-06 14:25:07 +0200

    removed config/db.php

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=marina',
-    'username' => 'marinauser',
-    'password' => '6AQaj6Jj',
-    'charset' => 'utf8',
-];

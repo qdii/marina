8213a862d70c0f43e6da515e5abe5454c9f962ef
Victor Lavaud (qdii) <victor.lavaud@gmail.com>
2015-07-18 16:51:28 +0200

    removed unused code

--- components/PriceComputer.php
+++ components/PriceComputer.php
@@ -32,32 +32,6 @@ class PriceComputer
 {
     public $ingredients     = [];
 
-    public function addCompositionItem(\app\models\Composition $item, $nbGuests)
-    {
-        assert( $nbGuests != 0 );
-
-        $ingredient = $item->getIngredient0()->one();
-        $quantity   = $item->quantity * $nbGuests;
-
-        if ( !isset( $this->ingredients[$ingredient->id]['quantity'] ) )
-            $this->ingredients[$ingredient->id]['quantity'] = 0;
-        else
-            $quantity += $this->ingredients[$ingredient->id]['quantity'];
-
-        $this->ingredients[$ingredient->id]['name']       = $ingredient->name;
-        $this->ingredients[$ingredient->id]['quantity']   = $quantity;
-        $this->ingredients[$ingredient->id]['price']      = $ingredient->price * $quantity;
-        $this->ingredients[$ingredient->id]['unitPrice']  = $ingredient->price;
-        $this->ingredients[$ingredient->id]['unitName']   = $ingredient->getUnit0()->one()->shortName;
-    }
-
-    private function _addDish(\app\models\Dish $dish, $nbGuests)
-    {
-        foreach ( $dish->getCompositions()->all() as $item ) {
-            $this->addCompositionItem($item, $nbGuests);
-        }
-    }
-
     /**
      * Return the dishes contained in this meal
      *

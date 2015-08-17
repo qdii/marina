2c69d065b06c89de1440e644b1a1e8b28c15822c
Victor Lavaud (qdii) <victor.lavaud@gmail.com>
2015-08-17 23:40:28 +0300

    added codeception test fixtures

---
+++ tests/codeception/fixtures/data/unit.php
@@ -0,0 +1,16 @@
+<?php
+return [
+    [
+        "id" => 1,
+        "display" => "ℓ",
+        "name" => "liter of water",
+        "weight" => 1000,
+    ],
+    [
+        "id" => 2,
+        "display" => "ℓ",
+        "name" => "liter of milk",
+        "weight" => 1030,
+    ],
+
+];

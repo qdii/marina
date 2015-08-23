88a34d9b93f2a492acbf4944158b52f8daf9652c
Victor Lavaud (qdii) <victor.lavaud@gmail.com>
2015-08-23 20:47:29 +0300

    improved tests

--- tests/codeception/acceptance/CalendarCept.php
+++ tests/codeception/acceptance/CalendarCept.php
@@ -5,4 +5,4 @@ use tests\codeception\_pages\CalendarPage;
 $I = new AcceptanceTester($scenario);
 $I->wantTo('ensure that calendar works');
 CalendarPage::openBy($I);
-$I->see('Calendar', 'h1');
+
--- tests/codeception/acceptance/RecipeCept.php
+++ tests/codeception/acceptance/RecipeCept.php
@@ -3,7 +3,10 @@
 use tests\codeception\_pages\RecipePage;
 
 $I = new AcceptanceTester($scenario);
-$I->wantTo('ensure that contact works');
+$I->wantTo('ensure that recipe works');
 
 $contactPage = RecipePage::openBy($I);
-
+$I->dontSee('Energy');
+$I->dontSee('Name');
+$I->dontSee('Quantity');
+$I->dontSee('Proteins');

15d7cdb1359f13e3551ab1bc2e614b85849d94f9
Victor Lavaud (qdii) <victor.lavaud@gmail.com>
2015-08-23 20:40:19 +0300

    more codeception tweaking

--- tests/codeception/acceptance/CalendarCept.php
+++ tests/codeception/acceptance/CalendarCept.php
@@ -3,6 +3,6 @@
 use tests\codeception\_pages\CalendarPage;
 
 $I = new AcceptanceTester($scenario);
-$I->wantTo('ensure that about works');
+$I->wantTo('ensure that calendar works');
 CalendarPage::openBy($I);
 $I->see('Calendar', 'h1');

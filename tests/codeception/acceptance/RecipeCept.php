61526f03580958d0ff08a8dd958f28ca82234f7f
Victor Lavaud (qdii) <victor.lavaud@gmail.com>
2015-09-04 15:34:51 +0200

    typo in test

--- tests/codeception/acceptance/RecipeCept.php
+++ tests/codeception/acceptance/RecipeCept.php
@@ -5,7 +5,7 @@ use tests\codeception\_pages\RecipePage;
 $I = new AcceptanceTester($scenario);
 $I->wantTo('ensure that recipe works');
 
-$contactPage = RecipePage::openBy($I);
+$recipe = RecipePage::openBy($I);
 $I->dontSee('Energy');
 $I->dontSee('Name');
 $I->dontSee('Quantity');

2c0b8a48e8e0960a92289b73d7489ff9e8cb3ab6
Victor Lavaud (qdii) <victor.lavaud@gmail.com>
2015-09-04 12:34:16 +0200

    improved product picker

--- components/ProductPicker.php
+++ components/ProductPicker.php
@@ -13,6 +13,8 @@
  */
 namespace app\components;
 use app\models\Product;
+use app\models\Proportion;
+use app\models\Fraction;
 
 /**
  * Selects products based on a list of ingredients
@@ -44,5 +46,56 @@ class ProductPicker
             ->rightJoin('fraction', 'fraction.product = product.id')
             ->where(['fraction.ingredient' => $ingred, 'product.vendor' => $vendor]);
         return $product->all();
+    }
+
+    /**
+     * Computes the amount of product that's necessary to match the given
+     * quantity of ingredient
+     *
+     * @param int   $ingr The ingredient id
+     * @param int   $prod The product id
+     * @param float $qty  The quantity of ingredient
+     * @param int   $unit The unit id of the quantity of ingredient
+     *
+     * @return float The amount of product necessary
+     */
+    public function getAmountOfProduct($ingr, $prod, $qty, $unit)
+    {
+        $couple     = ['ingredient' => $ingr, 'product' => $prod];
+        $proportion = Proportion::findOne($couple);
+        $fraction   = Fraction::findOne($couple);
+
+        // if the product does not contain any of the given ingredient
+        if (!$proportion) {
+            return 0;
+        }
+
+        // so let's say the product is a pack of veggies from Tesco
+        $qtyOfCarrotsInThePack = $proportion->weight;
+
+        // and we need 300g of carrots
+        $weNeedThatMuchCarrots = $qty * $fraction->fraction;
+
+        // so this is how many packs we needs
+        $howManyPacks = $weNeedThatMuchCarrots / $qtyOfCarrotsInThePack;
+
+        return ceil($howManyPacks);
+    }
+
+    /**
+     * Converts a given quantity of something in a given unit into the
+     * quantity in grams
+     *
+     * @param float $qty  The quantity of something
+     * @param int   $unit The id of an unit
+     *
+     * @return float The quantity in grams of the converted product
+     */
+    private function _convertUnitToGrams($qty, $unit)
+    {
+        // by default, units are expressed in grams
+        if (!$unit || $qty == 0) {
+            return $qty;
+        }
     }
 }
--- tests/codeception/unit/ProductPickerTest.php
+++ tests/codeception/unit/ProductPickerTest.php
@@ -23,4 +23,75 @@ class ProductPickerTest extends \Codeception\TestCase\Test
         $products   = $picker->pickProduct($ingredient, $vendor);
         $this->assertCount(6, $products);
     }
+
+    public function testAmountOfOneProduct()
+    {
+        $ingredient = 1212; // milk
+        $product    = 95;   // tin can of milk
+        $picker     = new ProductPicker;
+
+        $packs      = $picker->getAmountOfProduct(
+            $ingredient,
+            $product,
+            2000, /* we need 2 kg of milk */
+            null
+        );
+
+        // ok so a tin can has 340g of milk inside,
+        // so we need at least 6 packs
+        $this->assertEquals(6.0, $packs, 0.1);
+    }
+
+    public function testAmountOfNonMatchingProduct()
+    {
+        $ingredient = 2006; // spices
+        $product    = 95;   // tin can of milk
+        $picker     = new ProductPicker;
+
+        $packs      = $picker->getAmountOfProduct(
+            $ingredient,
+            $product,
+            2000, /* we need 2 kg of milk */
+            null
+        );
+
+        // we can't obtain spcies from cans of milk, so the result should be 0
+        $this->assertEquals(0, $packs);
+    }
+
+    public function testFractionedProduct()
+    {
+        $picker = new ProductPicker;
+
+        // ingredient
+        $tomatoes = 11529;
+
+        // products
+        $vineTomatoes     = 23; // on the vine
+        $everydayTomatoes = 75; // normal ones
+
+        $qty = 2000; // we want 2kg of tomatoes
+
+        // seulement 25% du produit doit être composé de vine tomatoes,
+        // 25% de 2000g sont 500g, et chaque pack de vine tomatoes en contient
+        // 70g (selon la table proportion), donc il nous faut 8 packs.
+        $vinePacks = $picker->getAmountOfProduct(
+            $tomatoes,
+            $vineTomatoes,
+            $qty,
+            null
+        );
+        $this->assertEquals(8, $vinePacks);
+
+        // 75% du produit doit être composé de tomates classiques, i.e. 1500g,
+        // et chaque pack fait 400g (cf table proportion), donc il nous
+        // 4 packs (1600g)
+        $everydayPacks = $picker->getAmountOfProduct(
+            $tomatoes,
+            $everydayTomatoes,
+            $qty,
+            null
+        );
+        $this->assertEquals(4, $everydayPacks);
+    }
 }

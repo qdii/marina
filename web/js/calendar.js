6fbe0b55751ae818856309852f1b0dc1eb9caaa9
Victor Lavaud (qdii) <victor.lavaud@gmail.com>
2015-09-16 00:06:40 +0200

    now hiding/showing shopping list

--- web/js/calendar.js
+++ web/js/calendar.js
@@ -11,6 +11,7 @@ var calendarProto = {
 
     set_boat_id: function(id) {
         $('#boat-name').val(id);
+        $('#boat-name').trigger('chosen:updated');
     },
 
     refresh_calendar: function() {
@@ -19,6 +20,7 @@ var calendarProto = {
 
     on_boat_change: function() {
         this.refresh_calendar();
+        this.refresh_shopping_list();
     },
 
     on_vendor_change: function() {
@@ -31,8 +33,17 @@ var calendarProto = {
     },
 
     refresh_shopping_list: function() {
+        var boat_id = this.get_boat_id();
+        if (boat_id === 0) {
+            this.hide_shopping_list();
+        } else {
+            this.show_shopping_list();
+        }
+
+        this.clear_shopping_list();
+
         var params = {
-            boatId: this.get_boat_id(),
+            boatId: boat_id
         };
 
         $.getJSON(window.fetch_ingredient_list_url, params, function(data) {
@@ -40,15 +51,27 @@ var calendarProto = {
         });
     },
 
+    clear_shopping_list: function() {
+        $('#shopping-list tbody').empty();
+    },
+
     write_shopping_list: function(data) {
         var list = $('#shopping-list tbody');
-        list.empty();
         $.each(data, function(id, val) {
             list.append('<tr data-id="' + id + '"><td>' + val.quantity +
                         '</td><td>' + val.name + '</td>');
         });
-    }
+    },
 
+    hide_shopping_list: function() {
+        $('#shopping-list').hide('slow');
+    },
+
+    show_shopping_list: function() {
+        $('#shopping-list').show('slow');
+    }
 };
 
 var cal = Object.create(calendarProto);
+cal.set_boat_id(0);
+cal.hide_shopping_list();

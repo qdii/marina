215f43eb2aef3f2bf6371aa53883e6048b3fa76f
Victor Lavaud (qdii) <victor.lavaud@gmail.com>
2015-09-23 22:40:46 +0200

    facebook id is now stored in a non-committed file

---
+++ views/site/invalid-password.php
@@ -0,0 +1,14 @@
+<?php
+$this->title = 'Error';
+?>
+
+<div class="page-header">
+    <h1><?php echo $this->title ?></h1>
+</div>
+
+<div class="row">
+    <div class="col-md-3"></div>
+    <div class="col-md-6 alert alert-danger">
+        Invalid user/password
+    </div>
+</div>

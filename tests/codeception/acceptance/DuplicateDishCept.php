fd7213732531e4cce1159b5f6b0df2bdf551cba2
Victor Lavaud (qdii) <victor.lavaud@gmail.com>
2015-10-01 14:54:29 +0200

    new test: duplicate dish

---
+++ tests/codeception/acceptance/DuplicateDishCept.php
@@ -0,0 +1,22 @@
+<?php
+
+use tests\codeception\_pages\RecipePage;
+use tests\codeception\_pages\LoginPage;
+
+$I = new AcceptanceTester($scenario);
+$I->wantTo('duplicate a recipe');
+
+$loginPage = LoginPage::openBy($I);
+$loginPage->login('admin', 'admin');
+
+$recipe = RecipePage::openBy($I);
+
+$I->click('Choose a dish');
+$I->click($recipe->selectionCrepes);
+$I->waitForText('Energy');
+
+$I->click('#copy-dish');
+$I->waitForText('Create a dish from selection');
+$I->fillField('input[name="Dish[name]"]', 'Test Dish');
+$I->click('#submit-copy');
+$I->dontSee('Create a dish from selection');

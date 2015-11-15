be27840b0da2613fb3d18e56f92b3812c524cc8f
Victor Lavaud (qdii) <victor.lavaud@gmail.com>
2015-11-15 23:24:09 +0000

    crud for course

---
+++ models/CourseSearch.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+use app\models\Course;
+
+/**
+ * CourseSearch represents the model behind the search form about `app\models\Course`.
+ */
+class CourseSearch extends Course
+{
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['id', 'type', 'meal', 'dish'], 'integer'],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function scenarios()
+    {
+        // bypass scenarios() implementation in the parent class
+        return Model::scenarios();
+    }
+
+    /**
+     * Creates data provider instance with search query applied
+     *
+     * @param array $params
+     *
+     * @return ActiveDataProvider
+     */
+    public function search($params)
+    {
+        $query = Course::find();
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        $this->load($params);
+
+        if (!$this->validate()) {
+            // uncomment the following line if you do not want to return any records when validation fails
+            // $query->where('0=1');
+            return $dataProvider;
+        }
+
+        $query->andFilterWhere([
+            'id' => $this->id,
+            'type' => $this->type,
+            'meal' => $this->meal,
+            'dish' => $this->dish,
+        ]);
+
+        return $dataProvider;
+    }
+}

d17f3a466c2c7bbcdd8acb1b3e8039462f9f0219
Victor Lavaud (qdii) <victor.lavaud@gmail.com>
2015-12-21 22:27:02 +0000

    added url cruise/

---
+++ views/site/cruise.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * This page lets the user look up a recipe in terms of products
+ *
+ * PHP version 5.4
+ *
+ * @category Views
+ * @package  Views
+ * @author   Victor Lavaud (qdii) <victor.lavaud@gmail.com>
+ * @license  GPLv2 https://www.gnu.org/licenses/gpl-2.0.html
+ * @link     http://marina.dodges.it
+ *
+ */
+
+$this->title = 'Cruises';
+?>
+
+<div class="page-header">
+<h1><?php echo $this->title ?></h1>
+</div>
+
+<div id="cruise_list">
+</div>
